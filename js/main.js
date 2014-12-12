4466e59c3e8fb5a5c4e28705e886eaa8363ac484
Adam Perlis <adam.perlis@gmail.com>
2014-12-12 18:05:28 -0500

    fixed a bunch of stuff

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(function(){
 
   var layout = 'list', // Store the current layout as a variable
-      $container = $('#Container'), // Cache the MixItUp container
+      $container = $('#libaryContainer'), // Cache the MixItUp container
       $changeLayout = $('#ChangeLayout'); // Cache the changeLayout button
   
   // Instantiate MixItUp with some custom options:
   
   $container.mixItUp({
     animation: {
-		animateChangeLayout: false, // Animate the positions of targets as the layout changes
-		animateResizeTargets: false, // Animate the width/height of targets as the layout changes
-		animateResizeContainer: false,
+		animateChangeLayout: true, // Animate the positions of targets as the layout changes
+		animateResizeTargets: true, // Animate the width/height of targets as the layout changes
+		animateResizeContainer: true,
 		duration: 400,
 		effects: 'fade stagger(34ms) translateY(10%)',
 		easing: 'ease'
@@ -19,33 +19,12 @@ $(function(){
       containerClass: 'list' // Add the class 'list' to the container on load
     }
   });
-  
-  // MixItUp does not provide a default "change layout" button, so we need to make our own and bind it with a click handler:
-  
-  $changeLayout.on('click', function(){
-    
-    // If the current layout is a list, change to grid:
-    
-    if(layout == 'list'){
-      layout = 'grid';
-      
-      $changeLayout.text('List'); // Update the button text
-      
-      $container.mixItUp('changeLayout', {
-        containerClass: layout // change the container class to "grid"
-      });
-      
-    // Else if the current layout is a grid, change to list:  
-    
-    } else {
-      layout = 'list';
-      
-      $changeLayout.text('Grid'); // Update the button text
-      
-      $container.mixItUp('changeLayout', {
-        containerClass: layout // Change the container class to 'list'
-      });
-    }
+
+  $('.tag').click(function(e){
+
+    // Cancel the default action
+    e.preventDefault();
   });
+
   
 });
