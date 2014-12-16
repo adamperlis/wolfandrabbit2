161c8c25f7be8bd2b044079e36434ea285e43bee
Adam Perlis <adam.perlis@gmail.com>
2014-12-16 15:16:03 -0500

    fixed SVG logo and script for animated nav

--- js/animated-nav.js
+++ js/animated-nav.js
@@ -8,15 +8,13 @@ $(document).ready(function () {
 	
 	//switch the navbar to white when it reaches a certain point
 	$(document).scroll(function(){
-    var bheight = $(window).height();
-    var percent = 0.9;
-    var hpercent = bheight * percent;
+    var scrollPos = 715;
 
-    if($(this).scrollTop() > hpercent){
+    if($(this).scrollTop() > scrollPos){
         $(".navbar-inverse").css({"background":"white","border-bottom":"1px solid lightgray"});
         $(".nav-menu-items li a").css({"color":"#444"});
         $(".navbar-nav li a:focus").css({"color":"#444"});
-        $(".navbar-brand img").css({"-webkit-filter":"invert(1)"});
+        $(".navbar-brand .logo").addClass("logo-inverse");
         $(".icon-bar").css({"background":"black"});
         $(".navbar-inverse .navbar-toggle:hover").css({"color":"black","background":"none","transform":"scale(1)"});
         $(".navbar-toggle.active .icon-bar:nth-of-type(2)").css({"background-color":"transparent","background":"none"});
@@ -24,7 +22,7 @@ $(document).ready(function () {
 	}else{
         $(".navbar-inverse").css({"background":"none","border-bottom":"0px solid lightgray"});
         $(".nav-menu-items li a").css({"color":"white"});
-        $(".navbar-brand img").css({"-webkit-filter":"invert(0)"});
+        $(".navbar-brand .logo").removeClass("logo-inverse");
         $(".icon-bar").css({"background":"white"});
         $(".navbar-inverse .navbar-toggle:hover").css({"color":"white","background":"none","transform":"scale(1)"});
         $(".navbar-toggle.active .icon-bar:nth-of-type(2)").css({"background-color":"white","background":"none"});
