820b58b2440bf47e5798d50c153a2605cc3f9afe
Adam Perlis <adam.perlis@gmail.com>
2014-12-22 22:26:12 -0500

    added audio player, fixed some issues with button hovers

---
+++ js/audio-player.js
@@ -0,0 +1,136 @@
+var music = document.getElementById('music'); // id for audio element
+var duration; // Duration of audio clip
+var pButton = document.getElementById('pButton'); // play button
+
+var playhead = document.getElementById('playhead'); // playhead
+
+var playBar = document.getElementById('playbar'); // playhead
+
+var timeline = document.getElementById('timeline'); // timeline
+// timeline width adjusted for playhead
+var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+
+// timeupdate event listener
+music.addEventListener("timeupdate", timeUpdate, false);
+
+//Makes timeline clickable
+timeline.addEventListener("click", function (event) {
+	moveplayhead(event);
+	music.currentTime = duration * clickPercent(event);
+}, false);
+
+// returns click as decimal (.77) of the total timelineWidth
+function clickPercent(e) {
+	return (event.pageX - timeline.offsetLeft) / timelineWidth;
+}
+
+// Makes playhead draggable 
+playhead.addEventListener('mousedown', mouseDown, false);
+window.addEventListener('mouseup', mouseUp, false);
+
+// Boolean value so that mouse is moved on mouseUp only when the playhead is released 
+var onplayhead = false;
+// mouseDown EventListener
+function mouseDown() {
+	onplayhead = true;
+	window.addEventListener('mousemove', moveplayhead, true);
+	music.removeEventListener('timeupdate', timeUpdate, false);
+}
+// mouseUp EventListener
+// getting input from all mouse clicks
+function mouseUp(e) {
+	if (onplayhead == true) {
+		moveplayhead(e);
+		window.removeEventListener('mousemove', moveplayhead, true);
+		// change current time
+		music.currentTime = duration * clickPercent(e);
+		music.addEventListener('timeupdate', timeUpdate, false);
+	}
+	onplayhead = false;
+}
+// mousemove EventListener
+// Moves playhead as user drags
+function moveplayhead(e) {
+	var newMargLeft = e.pageX - timeline.offsetLeft;
+	if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+		playhead.style.marginLeft = newMargLeft + "px";
+	}
+	if (newMargLeft < 0) {
+		playhead.style.marginLeft = "0px";
+	}
+	if (newMargLeft > timelineWidth) {
+		playhead.style.marginLeft = timelineWidth + "px";
+	}
+}
+
+// timeUpdate 
+// Synchronizes playhead position with current point in audio 
+function timeUpdate() {
+	var playPercent = timelineWidth * (music.currentTime / duration);
+	playhead.style.marginLeft = playPercent + "px";
+	playBar.style.paddingLeft = playPercent + "px";
+	if (music.currentTime == duration) {
+		pButton.className = "";
+		pButton.className = "play";
+	}
+}
+
+//Play and Pause
+function play() {
+	// start music
+	$("#pButton").removeClass("entypo-play");
+	if (music.paused) {
+		music.play();
+		// remove play, add pause
+		pButton.className = "";
+		pButton.className = "entypo-pause";
+	} else { // pause music
+		music.pause();
+		// remove pause, add play
+		pButton.className = "";
+		pButton.className = "play";
+	}
+}
+
+// Gets audio file duration
+music.addEventListener("canplaythrough", function () {
+	duration = music.duration;  
+}, false);
+
+
+function formatSecondsAsTime(secs, format) {
+  var hr  = Math.floor(secs / 3600);
+  var min = Math.floor((secs - (hr * 3600))/60);
+  var sec = Math.floor(secs - (hr * 3600) -  (min * 60));
+
+  if (min < 10){ 
+    min = "0" + min; 
+  }
+  if (sec < 10){ 
+    sec  = "0" + sec;
+  }
+
+  return min + ':' + sec;
+}
+
+function updateTrackTime(track){
+  var currTimeDiv = document.getElementById('currentTime');
+  var durationDiv = document.getElementById('duration');
+
+  var currTime = Math.floor(track.currentTime).toString(); 
+  var duration = Math.floor(track.duration).toString();
+
+  currTimeDiv.innerHTML = formatSecondsAsTime(currTime);
+  durationDiv.innerHTML = formatSecondsAsTime(duration);
+
+  if (isNaN(duration)){
+    durationDiv.innerHTML = '02:00';
+    currTimeDiv.innerHTML = '00:00';
+  } 
+  else{
+    durationDiv.innerHTML = formatSecondsAsTime(duration);
+  }
+}
+
+
+
--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,15 @@
 var jumboHeight = $('.jumbotron').outerHeight();
+var jumboHeight2 = $('.jumbotron2').outerHeight();
 function parallax(){
     var scrolled = $(window).scrollTop();
     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    
+    if($(window).scrollTop() > 1000 ){
+    	$(".bg-2").fadeIn("slow");
+    	$(".bg-2").css('height', (1805 + jumboHeight2-scrolled) + 'px');
+    } else{
+		$(".bg-2").fadeOut('slow');
+	}
 }
 
 $(window).scroll(function(e){
